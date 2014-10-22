97df1ef2928c1b18cf69d6260dad2028e79a7289
hcjcch <huangchen@mail.bistu.edu.cn>
2014-10-22 20:32:19 +0800

    first commit

---
+++ src/main/java/com/hcjcch/educationaladministration/config/StaticVariable.java
@@ -0,0 +1,10 @@
+package com.hcjcch.educationaladministration.config;
+
+/**
+ * Created by hcjcch on 2014/9/15.
+ */
+public class StaticVariable {
+    public static final String client_secret = "b4cfaf0479f89fd48a508de762da5508";
+    public static final String client_id = "a4ccaed4332180b3462c7fe1bdb79a77";
+    public static final String authorizeURL = "https://222.249.250.89:8443/oauth/authorize";
+}
---
+++ src/main/java/com/hcjcch/educationaladministration/educational/MainActivity.java
@@ -0,0 +1,71 @@
+package com.hcjcch.educationaladministration.educational;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.hcjcch.educationaladministration.event.NetworkChangeEvent;
+import com.hcjcch.educationaladministration.oauth.OauthActivity;
+import com.hcjcch.educationaladministration.oauth.OauthActivity_;
+
+import de.greenrobot.event.EventBus;
+
+
+public class MainActivity extends Activity {
+    private Button mark;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*mark = (Button)findViewById(R.id.mark);
+        mark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, OauthActivity_.class);
+                startActivity(intent);
+            }
+        });*/
+        EventBus.getDefault().register(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void onEventMainThread(NetworkChangeEvent event){
+        if (event.isNetworkConnected()){
+            Toast.makeText(this,"网络连接",Toast.LENGTH_SHORT).show();
+        }  else {
+            Toast.makeText(this,"网络断开",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+}
