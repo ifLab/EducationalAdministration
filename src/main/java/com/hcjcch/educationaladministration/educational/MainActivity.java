9d13fbedb283aaee849d70021eee43c10685c58e
hcjcch <huangchen@mail.bistu.edu.cn>
2014-10-26 20:46:10 +0800

    Add Oauth

--- src/main/java/com/hcjcch/educationaladministration/educational/MainActivity.java
+++ src/main/java/com/hcjcch/educationaladministration/educational/MainActivity.java
@@ -1,12 +1,15 @@
 package com.hcjcch.educationaladministration.educational;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.hcjcch.educationaladministration.activity.OauthActivity;
 import com.hcjcch.educationaladministration.event.NetworkChangeEvent;
 
 import de.greenrobot.event.EventBus;
@@ -19,15 +22,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*mark = (Button)findViewById(R.id.mark);
+        mark = (Button)findViewById(R.id.mark);
         mark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, OauthActivity_.class);
+                intent.setClass(MainActivity.this, OauthActivity.class);
                 startActivity(intent);
             }
-        });*/
+        });
         EventBus.getDefault().register(this);
     }
 
